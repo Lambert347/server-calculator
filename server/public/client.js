e0836d1b3a46d7f1a8267960218277dd5e929441
Ian Lambert <lambe347@umn.edu>
2021-04-17 15:15:30 -0500

    operation buttons working

--- server/public/client.js
+++ server/public/client.js
@@ -1,7 +1,39 @@
+
 console.log('JAVAJAVAJAVA');
+
+let numObject = {
+        operation: null
+    }
 
 $(document).ready(readyOn);
 
 function readyOn() {
     console.log('JQ');
+
+    $('#addition').on('click', function(event){
+        numObject.operation = "add";
+        event.preventDefault();
+        console.log(numObject);
+        return numObject;
+    })
+    $('#subtraction').on('click', function(event){
+        numObject.operation = "subtract";
+        event.preventDefault();
+        console.log(numObject);
+        return numObject;
+    })
+    $('#multiplication').on('click', function(event){
+        numObject.operation = "multiply";
+        event.preventDefault();
+        console.log(numObject);
+        return numObject;
+    })
+    $('#division').on('click', function(event){
+        numObject.operation = "divide";
+        event.preventDefault();
+        console.log(numObject);
+        return numObject;
+    })
+
+
 }
